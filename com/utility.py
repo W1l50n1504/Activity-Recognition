34da6f43d25d39937ebf3bdac4e928e68b4963f5
w1l50n1504 <davide.paduanelli@gmail.com>
2021-07-22 16:57:54 +0200

    Sistemazione dati

--- com/utility.py
+++ com/utility.py
@@ -94,9 +94,11 @@ TrainingValAucBLSTM = absPath_ + '/graphs/blstm/trainingValAucBLSTM.png'
 ModelLossBLSTM = absPath_ + '/graphs/blstm/modelLossBLSTM.png'
 
 # dizionari riguardanti le attività registrate dai dataset
-labelDictUCI = {'WALKING': 0, 'WALKING_UPSTAIRS': 1, 'WALKING_DOWNSTAIRS': 2,
-                'SITTING': 3, 'STANDING': 4, 'LAYING': 5}
+labelDictUCI = {'WALKING': 0, 'WALKING_UPSTAIRS': 1, 'WALKING_DOWNSTAIRS': 2, 'SITTING': 3, 'STANDING': 4, 'LAYING': 5}
 
+labelDictMotionSense = {'WALKING': 0, 'WALKING_UPSTAIRS': 1, 'WALKING_DOWNSTAIRS': 2, 'SITTING': 3, 'STANDING': 4}
+
+# TODO elimina
 labelDictWISDM = {'Walking': 0, 'Upstairs': 1, 'Downstairs': 2, 'Sitting': 3, 'Standing': 4, 'Jogging': 6}
 
 labelDictUMAFALL = {'Walking': 0, 'Laying': 5, 'Jogging': 6, 'Falling': 7}
@@ -165,117 +167,7 @@ def get_human_dataset():
     return X, y
 
 
-# funzioni utili per il caricamento di MotionSense dataset
-
-
-def get_ds_infos():
-    """
-    Read the file includes data subject information.
-
-    Data Columns:
-    0: code [1-24]
-    1: weight [kg]
-    2: height [cm]
-    3: age [years]
-    4: gender [0:Female, 1:Male]
-
-    Returns:
-        A pandas DataFrame that contains inforamtion about data subjects' attributes
-    """
-
-    dss = pd.read_csv(motionPath + "data_subjects_info.csv")
-    print("[INFO] -- Data subjects' information is imported.")
-
-    return dss
-
-
-def set_data_types(data_types=["userAcceleration"]):
-    """
-    Select the sensors and the mode to shape the final dataset.
-
-    Args:
-        data_types: A list of sensor data type from this list: [attitude, gravity, rotationRate, userAcceleration]
-
-    Returns:
-        It returns a list of columns to use for creating time-series from files.
-    """
-    dt_list = []
-    for t in data_types:
-        if t != "attitude":
-            dt_list.append([t + ".x", t + ".y", t + ".z"])
-        else:
-            dt_list.append([t + ".roll", t + ".pitch", t + ".yaw"])
-
-    return dt_list
-
-
-def creat_time_series(dt_list, act_labels, trial_codes, mode="mag", labeled=True):
-    """
-    Args:
-        dt_list: A list of columns that shows the type of data we want.
-        act_labels: list of activites
-        trial_codes: list of trials
-        mode: It can be "raw" which means you want raw data
-        for every dimention of each data type,
-        [attitude(roll, pitch, yaw); gravity(x, y, z); rotationRate(x, y, z); userAcceleration(x,y,z)].
-        or it can be "mag" which means you only want the magnitude for each data type: (x^2+y^2+z^2)^(1/2)
-        labeled: True, if we want a labeld dataset. False, if we only want sensor values.
-
-    Returns:
-        It returns a time-series of sensor data.
-
-    """
-    num_data_cols = len(dt_list) if mode == "mag" else len(dt_list * 3)
-
-    if labeled:
-        dataset = np.zeros((0, num_data_cols + 7))  # "7" --> [act, code, weight, height, age, gender, trial]
-    else:
-        dataset = np.zeros((0, num_data_cols))
-
-    ds_list = get_ds_infos()
-
-    print("[INFO] -- Creating Time-Series")
-    for sub_id in ds_list["code"]:
-        for act_id, act in enumerate(act_labels):
-            for trial in trial_codes[act_id]:
-                fname = motionPath + 'A_DeviceMotion_data/' + act + '_' + str(trial) + '/sub_' + str(
-                    int(sub_id)) + '.csv'
-                raw_data = pd.read_csv(fname)
-                raw_data = raw_data.drop(['Unnamed: 0'], axis=1)
-                vals = np.zeros((len(raw_data), num_data_cols))
-                for x_id, axes in enumerate(dt_list):
-                    if mode == "mag":
-                        vals[:, x_id] = (raw_data[axes] ** 2).sum(axis=1) ** 0.5
-                    else:
-                        vals[:, x_id * 3:(x_id + 1) * 3] = raw_data[axes].values
-                    vals = vals[:, :num_data_cols]
-                if labeled:
-                    lbls = np.array([[act_id,
-                                      sub_id - 1,
-                                      ds_list["weight"][sub_id - 1],
-                                      ds_list["height"][sub_id - 1],
-                                      ds_list["age"][sub_id - 1],
-                                      ds_list["gender"][sub_id - 1],
-                                      trial
-                                      ]] * len(raw_data))
-                    vals = np.concatenate((vals, lbls), axis=1)
-                dataset = np.append(dataset, vals, axis=0)
-    cols = []
-    for axes in dt_list:
-        if mode == "raw":
-            cols += axes
-        else:
-            cols += [str(axes[0][:-2])]
-
-    if labeled:
-        cols += ["act", "id", "weight", "height", "age", "gender", "trial"]
-
-    dataset = pd.DataFrame(data=dataset, columns=cols)
-    return dataset
-
-
 # funzioni da utilizzare per selezionare le feature dai vari dataset
-
 def reduceSample(Xdf, yDf):
     # riduce la frequenza dei campioni da 100 Hz a 50Hz, da utilizzare con KUHAR
     reduce = pd.DataFrame(
@@ -328,8 +220,6 @@ def loadNmerge(X_df, Y_df, path, label):
 
     df = pd.read_csv(umafallPath + path, header=None, names=columnsUMAFALL, sep=';')
 
-    # ho preso solo le misurazioni dell'accelerometro e della posizione che mi interessa prendere anche il giroscopio 1
-    # TODO bisogna creare due dataset uno che contiene i dati relativi all'accelerometro e l'altro riguardante il giroscopio
     df = df.loc[(df['Sensor Type'] == 0) & (df['Sensor ID'] == 0)]
     finalDf = pd.DataFrame(columns=finalColumns)
 
@@ -386,10 +276,20 @@ def loadUCIHAR():
 
 
 def loadMotionSense():
-    #dataset finali che conterranno i dati per come ci servono
+    # dataset finali che conterranno i dati per come ci servono
     X_df = pd.DataFrame(columns=finalColumns, dtype='float32')
     Y_df = pd.DataFrame(columns=activity, dtype='int32')
-    return X_df.copy(), Y_df.copy()
+    labelDictMotionSense = {}
+
+    dws = 'com/dataset/MotionSense/dws/sub_1.csv'
+    jog = 'com/dataset/MotionSense/jog/sub_1.csv'
+    dws = 'com/dataset/MotionSense/sit/sub_1.csv'
+    dws = 'com/dataset/MotionSense/ups/sub_1.csv'
+    dws = 'com/dataset/MotionSense/wlk/sub_1.csv'
+
+
+    MSdataset = pd.read_csv(motionPath + dws)
+    # return X_df.copy(), Y_df.copy()
 
 
 def loadKUHAR():
@@ -438,6 +338,8 @@ def loadSavedData():
     # x = torch.tensor(x.values)
 
     return x, y
+
+
 '''
 robe che probabilmente non servono
     ACT_LABELS = ["dws", "ups", "wlk", "jog", "std", "sit"]
@@ -466,6 +368,6 @@ robe che probabilmente non servono
 '''
 
 if __name__ == '__main__':
-    #x, y = loadMotionSense()
+    # x, y = loadMotionSense()
     print('Hello this is Utility')
-
+    loadMotionSense()
